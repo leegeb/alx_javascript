bdfc5cb92e5b480d0d88b47fb7a1da0f41589be1
lydia gebre <lydiakgebre@gmail.com>
2023-09-06 03:55:40 +0400

    Update 2-rectangle.js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 class Rectangle {
     constructor(w, h) {
         if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-            return {};
+            return new Rectangle(0, 0);
         }
         this.width = w;
         this.height = h;
