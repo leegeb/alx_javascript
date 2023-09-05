fa4fac0f964a7122b40619f2234edb63acf349bd
lydia gebre <lydiakgebre@gmail.com>
2023-09-06 03:47:06 +0400

    Rectangle#2

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,5 +1,8 @@
 class Rectangle {
-    constructor(w, h){
+    constructor(w, h) {
+        if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+            return {};
+        }
         this.width = w;
         this.height = h;
     }
