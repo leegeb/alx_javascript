aa725f2a4a22ee5d385644e8a6047d62fc69b8c2
lydia gebre <lydiakgebre@gmail.com>
2023-09-06 21:58:24 +0400

    Handling invalid input for w , h

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,8 +2,9 @@
 module.exports = class Rectangle {
     constructor(w, h) {
         if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-            return new Rectangle(0, 0);
-        } {
+            this.width = 0;
+            this.height = 0;
+        }else {
             this.width = w;
             this.height = h;
         }
