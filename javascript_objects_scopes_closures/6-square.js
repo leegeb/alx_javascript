339cda42af1b70532caeff1dc016525ea94e9138
lydia gebre <lydiakgebre@gmail.com>
2023-09-07 22:35:21 +0400

    JavaScript - Objects, Scopes and Closures

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+module.exports =class Square extends require('./5-square.js'){
+    constructor(size){
+        super(size,size);
+
+    }
+
+    charPrint(c){
+        if (c == undefined){super.print()}
+        else{
+        for(let i=0;i<this.height;i++){
+            for(let i=0;i<this.width;i++){
+                process.stdout.write(c);
+            }
+            console.log(); 
+        }
+    }
+    }
+}
