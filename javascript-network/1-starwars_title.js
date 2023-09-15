19e781c52b4948f6b7571e3ecad7db2ba9fa254c
lydia gebre <lydiakgebre@gmail.com>
2023-09-16 00:34:54 +0400

    starwars-title

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
 
-function fetchMovieTitle(movieId) {
-    const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-}
-request(url, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-        const movieData = JSON.parse(body);
-        console.log(`Title of Episode ${movieData.episode_id}: ${movieData.title}`);
-    } else {
-        console.error('Error fetching movie data:', error);
-    }
-});
+const url = 'https://swapi-api.alx-tools.com/api/films/'+process.argv[2];
+
+request.get(url, (error, response, body) => {
+    if (error) 
+        console.log(error);
+      else {
+        const responseData = JSON.parse(body);
+        console.log(responseData.title);
+      }
+    });
