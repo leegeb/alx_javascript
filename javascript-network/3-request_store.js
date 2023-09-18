ea8b507d34f71596c2655bf0df4b02597dca6e84
lydia gebre <lydiakgebre@gmail.com>
2023-09-18 11:58:59 +0400

    Loripsum

---
+++ javascript-network/3-request_store.js
@@ -0,0 +1,18 @@
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const file = process.argv[3];
+
+
+const writeStream = fs.createWriteStream(file, { encoding: 'utf8' });
+
+
+request.get(url, { encoding: 'utf-8' })
+    .on('error', (err) => {
+        console.error(err);
+    })
+    .pipe(writeStream)
+    .on('finish', () => {
+        console.log(`Web page contents have been saved to ${file}`);
+    });
