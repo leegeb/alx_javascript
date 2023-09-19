7f5d795a1eb7c92c03d948c663e86434b170ce89
lydia gebre <lydiakgebre@gmail.com>
2023-09-20 00:07:48 +0400

    Update 4-completed_tasks.js

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -1,25 +1,27 @@
 #!/usr/bin/node
-const request = require('request');
+
+const req = require('request');
 const url = process.argv[2];
 const completed = {};
 
-request.get(url, (error, response, body) => {
-    if (error) {
-        console.log(error);
-    } else {
-        response = JSON.parse(body);
-        for (let i = 0; i < response.length; i++) {
+req.get(url, (err, res, body) => {
+    if (err) 
+    console.log(err);
+    else{
+        response =JSON.parse(body);
+
+        for(let i=0;i<response.length;i++){
             let count = 0;
-            let compare = response[i].userId;
-            for (let j = 0; j < response.length; j++) {
-                if (response[j].userId == compare && response[j].completed == true) {
+            let x =response[i].userId;
+            
+            for(let j=0;j<response.length;j++){
+                if (response[j].userId == x && response[j].completed == true)
                     count++;
-                }
-                if (count != 0) {
-                    completed[x] = count;
-                }
-                console.log(completed);
+
             }
+            if (count !=0)
+                completed[x]=count;
         }
+        console.log(completed);
     }
 });
