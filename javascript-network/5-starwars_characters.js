c848917a62edfe39c0e57379311b1312463a6af3
lydia gebre <lydiakgebre@gmail.com>
2023-09-20 00:22:24 +0400

    Update 5-starwars_characters.js

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
 
 const req = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/'+process.argv[2];
 
-require.get(url, (err, response, body) => {
-    if (err)
-        console.log(err);
-    else {
-        response = JSON.parse(body);
+req.get(url, (err, res, body) => {
+    if (err) 
+    console.log(err);
+    else{
+        response =JSON.parse(body);
         let cast = response.characters;
-        for (let i = 0; i < cast.length; i++) {
+        for(let i = 0; i<cast.length; i++){
 
-            req.get(cast[i], (error, response, bod) => {
-                if (error)
-                    console.log(error);
-                else {
-                    response = JSON.parse(bod);
+            req.get(cast[i], (error, ress, bod) => {
+                if (error) 
+                console.log(error);
+                else{
+                    resp =JSON.parse(bod);
                     console.log(resp.name);
                 }
             });
