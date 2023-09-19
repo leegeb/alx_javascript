a147ca921d01b4fc68bb8eeda2d1a4fdd19cdcd3
lydia gebre <lydiakgebre@gmail.com>
2023-09-20 03:07:58 +0400

    Javascript advanced

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,16 @@
+function createClassRoom (numbersOfStudents){
+    function studentSeat(seat){
+        return function(){
+            return seat;
+        };
+    }
+    let students = [];
+    for(i = 0; i <= numbersOfStudents; i++){
+        students.push(studentSeat(i + 1));
+    }
+    return students;
+}
+let classRoom = createClassRoom(10);
+console.log(classRoom[0]());
+console.log(classRoom[3]());
+console.log(classRoom[9]());
